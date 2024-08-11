2e40777ea6734137c5c137abe981f8fc25b8b815
Lakshit.ameta <lakshit.ameta@Kadellabs.com>
2024-08-11 12:25:05 +0530

    Install Stream Chat SDK

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,9 @@
 //Defines the global providers here.
 
 import { Slot, Stack } from "expo-router";
-
-export default function RootLayout(){
-    return <Slot/>; 
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+export default function RootLayout() {
+    return <GestureHandlerRootView className="flex-1">
+              <Slot />
+        </GestureHandlerRootView>;
 }
